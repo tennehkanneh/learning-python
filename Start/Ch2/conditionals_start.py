06fb78692551ec16a25a5be0b28abf588e0110d7
Tenneh (ten - nay) <154372867+tennehkanneh@users.noreply.github.com>
2025-06-27 18:00:32 +0000

    LinedIn Learning - Learning Python - 2. Basics of Python - Conditional Decisions

--- Start/Ch2/conditionals_start.py
+++ Start/Ch2/conditionals_start.py
@@ -2,8 +2,17 @@
 # Example file for working with conditional statements
 
 
-x, y = 10, 100
+x, y = 100,100 #declaring two variables on to the same line
+
 
 # conditional flow uses if, elif, else
+if x < y: 
+  print("x is less than y")
+elif x > y :
+  print("x is greater than y")
+else: 
+  print("x and y are the same")
 
 # conditional statements let you use "a if C else b"
+result = "x is less than y" if (x < y) else "x is greater or equalt to y"
+print(result)
