e3f31c45f0d868571820e86c97b425f0e2f1d18c
Tenneh (ten - nay) <154372867+tennehkanneh@users.noreply.github.com>
2025-06-27 17:19:48 +0000

    LinkedIn Learnding - Learning Python - 2. Basics of Python - Building 'Hello World'

--- Start/Ch2/helloworld_start.py
+++ Start/Ch2/helloworld_start.py
@@ -1,2 +1,10 @@
 # LinkedIn Learning Python course by Joe Marini
 # Example file for HelloWorld
+
+print("Hello World")
+
+# Create a variable called name that takes in a user input
+name = input("What is your name? ")
+
+# Concatenation in a print statment
+print("Nice to meet you", name)
