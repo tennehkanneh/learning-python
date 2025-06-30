effaa1907dde1c11447c43e71bca8bca3336c81e
Tenneh (ten - nay) <154372867+tennehkanneh@users.noreply.github.com>
2025-06-30 16:29:46 +0000

    challenge update

--- Start/Ch2/number_counter.py
+++ Start/Ch2/number_counter.py
@@ -2,22 +2,22 @@ numbers = [7, 17, 2, 13, 19, 20, 0, 5, 11, 1280, 105]
 
 
 def count_numbers(which, numbers):
+  if which != "even" and which != "odd":
+    print("Incorrect 'which' parameter count: -1")
+    return -1
+  
+  
+  
   count = 0
-
   for x in numbers:
-    if which == "even":
-      count = count + 1 if (x % 2) == 0 else  count
+    if which == "even" and (x % 2) == 0:
+      count += 1
         
-      
-    elif which == "odd":
-      count = count + 1 if (x % 2) != 0 else count
-      
-    else:
-      print("Incorrect 'which' parameter count: -1")
-      return -1
-    
+    if which == "odd" and (x % 2) != 0:
+      count += 1
+       
   print("Even count:" if which == "even" else "Odd count:", count)
-  return count 
+  return count
   
     
 
