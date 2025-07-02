c0d8476bbad6c733b20239faeb76c4469a963f95
Tenneh (ten - nay) <154372867+tennehkanneh@users.noreply.github.com>
2025-07-02 17:26:15 +0000

    LinkedIn Learning - Learning Python - 3. Beyond the Basics - The date, time, and datetime classes

--- Start/Ch5/dates_start.py
+++ Start/Ch5/dates_start.py
@@ -9,16 +9,23 @@ from datetime import datetime
 
 ## DATE OBJECTS
 # Get today's date from the simple today() method from the date class
-
+today = date.today()
+print("Today's date is", today)
 
 # print out the date's individual components
-
+print("Date Components:", today.day, today.month, today.year)
 
 # retrieve today's weekday (0=Monday, 6=Sunday)
+print("Today's Weekday #:", today.weekday())
 
+days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+print("Which is a", days[today.weekday()])
 
 ## DATETIME OBJECTS
 # Get today's date from the datetime class
-
+today = datetime.now()
+print("The current date and time is", today)
 
 # Get the current time
+t = datetime.time(datetime.now())
+print("The current time is", t)
